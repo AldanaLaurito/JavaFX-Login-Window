5c4be0e1762d7126cec50dd75bfbf60ba4739a32
AldanaLaurito <aldana.laurito.AL@gmail.com>
2020-10-21 20:00:05 -0300

    Added close action for cancel button

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+D
+src/sample/Main.java,a\d\ad8e8bd4ff8f02a914e3d9ca1bcc32dfc8c503a9
+F
+src/sample/sample.fxml,2\f\2f769fc3b0dd177c3365dff558843905f8609d7b
+J
+src/sample/Controller.java,a\9\a952ee24ccaf329067caf45d8e61416ce808b0ea
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,6 +14,12 @@ public class Controller implements Initializable {
     @FXML
     private TextField username;
 
+    @FXML
+    private TextField password;
+
+    @FXML
+    public Button cancelButton;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     }
@@ -20,5 +27,11 @@ public class Controller implements Initializable {
     public void okAction(ActionEvent event){
         System.out.println("Clicked Ok");
         System.out.println("username: "+ username.getText());
+        System.out.println("password: "+ password.getText());
+    }
+
+    public void cancelAction(ActionEvent event){
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
     }
 }

07e4743973b172303d266c451acb10194364ee23
AldanaLaurito <aldana.laurito.AL@gmail.com>
2020-10-21 16:17:55 -0300

    Added action for Ok button

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private TextField username;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    public void okAction(ActionEvent event){
+        System.out.println("Clicked Ok");
+        System.out.println("username: "+ username.getText());
+    }
 }
